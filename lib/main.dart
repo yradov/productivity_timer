fe08c5108e5e9b1266c3dbe6b96cfb148c0800bf
RYuriy <radov.yuriy@ukr.net>
2021-12-04 21:51:21 +0200

    --INITIAL--

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Work Timer"),
+        ),
+        body: Center(
+          child: Text("My Work Timer"),
+        ),
+      ),
+    );
+  }
+}
+
