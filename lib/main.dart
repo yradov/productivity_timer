4e31115d2a60b6b6ab0b5818c8f988c7b0c952a2
RYuriy <radov.yuriy@ukr.net>
2021-12-05 02:46:15 +0200

    + first row with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:productivity_timer/widgets.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -16,20 +17,70 @@ class MyApp extends StatelessWidget {
       home: const TimerHomePage(),
     );
   }
-} //MyApp
+} // MyApp
 
 class TimerHomePage extends StatelessWidget {
   const TimerHomePage({Key? key}) : super(key: key);
 
+  final double defaultPadding = 5.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Work Timer"),
+      appBar: AppBar(
+        title: const Text("My Work Timer"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: const Color(0xff009688),
+                    text: 'Work',
+                    size: 0,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: const Color(0xff607D8B),
+                    text: 'Short Break',
+                    size: 150,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: const Color(0xff455A64),
+                    text: 'Long Break',
+                    size: 150,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+              ],
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(),
-        ),
-      );
+      ),
+    );
   }
-}
+
+  void emptyMethod() {}
+}// TimerHomePage
