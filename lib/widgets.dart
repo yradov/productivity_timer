a1ce6585203240542d0d8d8a4ec31b7712acfc85
RYuriy <radov.yuriy@ukr.net>
2021-12-05 02:01:18 +0200

    + ProductivityButton separate class

---
+++ lib/widgets.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProductivityButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double size;
+  final VoidCallback onPressed;
+
+  const ProductivityButton(
+      {Key? key,
+      required this.color,
+      required this.text,
+      required this.size,
+      required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white),
+      ),
+      onPressed: onPressed,
+      color: color,
+      minWidth: size,
+    );
+  }
+}
